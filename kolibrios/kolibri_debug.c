12b929cf9043c2e370b43f139dffb12b60769979
Ashish Gupta <ashmew2@gmail.com>
2016-02-20 19:20:21 +0000

    Add C file for debug

---
+++ kolibrios/kolibri_debug.c
@@ -0,0 +1,15 @@
+#include "kolibrios/kolibri_debug.h"
+/* Write a printf() like function (variable argument list) for 
+   writing to debug board */
+
+void debug_board_write_byte(const char ch){
+    __asm__ __volatile__(
+    "int $0x40"
+    :
+    :"a"(63), "b"(1), "c"(ch));
+}
+
+void debug_board_write_str(const char* str){
+  while(*str)
+    debug_board_write_byte(*str++);
+}
