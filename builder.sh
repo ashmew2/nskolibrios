5bd950d98448818d3d40039b4b921077dffc2e7a
Ashish Gupta <ashmew2@gmail.com>
2016-02-28 18:29:44 +0000

    Fix kos32-ld linker operation

--- builder.sh
+++ builder.sh
@@ -3,7 +3,8 @@ make TARGET=kolibrios
 cd build-Linux-kolibrios
 
 cp ../kolibrios/*obj .
-kos32-ld -L/home/autobuild/tools/win32/mingw32/lib *.o ~/ofiles/*.o *.obj -lz -dll -lapp -lc -lgcc -o ../_netsurf
+
+kos32-ld *.o ~/ofiles/*.o *.obj -T/home/bob/kolibrios/contrib/sdk/sources/newlib/libc/app.lds -nostdlib -L/home/autobuild/tools/win32/mingw32/lib -lgcc /home/autobuild/tools/win32/lib/libz.a /home/autobuild/tools/win32/lib/libdll.a /home/autobuild/tools/win32/lib/libapp.a /home/autobuild/tools/win32/lib/libc.dll.a -o netsurfe
 
 echo "Ignore the errors above (for now...)"
 echo "_netsurf is your executable. Enjoy."
