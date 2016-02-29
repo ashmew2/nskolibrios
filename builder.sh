47ad0fa0e985f20928061cfff7c154e79fa67913
Ashish Gupta <ashmew2@gmail.com>
2016-02-29 06:24:34 +0000

    Add image base = 0 and static to ld line

--- builder.sh
+++ builder.sh
@@ -4,7 +4,7 @@ cd build-Linux-kolibrios
 
 cp ../kolibrios/*obj .
 
-kos32-ld *.o ~/ofiles/*.o *.obj -T/home/bob/kolibrios/contrib/sdk/sources/newlib/libc/app.lds -nostdlib -L/home/autobuild/tools/win32/mingw32/lib -lgcc /home/autobuild/tools/win32/lib/libz.a /home/autobuild/tools/win32/lib/libdll.a /home/autobuild/tools/win32/lib/libapp.a /home/autobuild/tools/win32/lib/libc.dll.a -o netsurfe
+kos32-ld *.o ~/ofiles/*.o *.obj -T/home/bob/kolibrios/contrib/sdk/sources/newlib/libc/app.lds -nostdlib -static --image-base 0 -L/home/autobuild/tools/win32/mingw32/lib -lgcc /home/autobuild/tools/win32/lib/libz.a /home/autobuild/tools/win32/lib/libdll.a /home/autobuild/tools/win32/lib/libapp.a /home/autobuild/tools/win32/lib/libc.dll.a -o netsurfe
 
 echo "Ignore the errors above (for now...)"
 echo "_netsurf is your executable. Enjoy."
