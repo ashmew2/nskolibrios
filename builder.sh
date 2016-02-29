3a7511f894bfc16a865d14c6d0736a0fa0305a40
Ashish Gupta <ashmew2@gmail.com>
2016-02-29 06:29:49 +0000

    kolibrios executables running!

--- builder.sh
+++ builder.sh
@@ -4,7 +4,14 @@ cd build-Linux-kolibrios
 
 cp ../kolibrios/*obj .
 
-kos32-ld *.o ~/ofiles/*.o *.obj -T/home/bob/kolibrios/contrib/sdk/sources/newlib/libc/app.lds -nostdlib -static --image-base 0 -L/home/autobuild/tools/win32/mingw32/lib -lgcc /home/autobuild/tools/win32/lib/libz.a /home/autobuild/tools/win32/lib/libdll.a /home/autobuild/tools/win32/lib/libapp.a /home/autobuild/tools/win32/lib/libc.dll.a -o netsurfe
+kos32-ld *.o ~/ofiles/*.o *.obj -T/home/bob/kolibrios/contrib/sdk/sources/newlib/libc/app.lds -nostdlib -static --image-base 0 -L/home/autobuild/tools/win32/mingw32/lib -lgcc /home/autobuild/tools/win32/lib/libz.a /home/autobuild/tools/win32/lib/libdll.a /home/autobuild/tools/win32/lib/libapp.a /home/autobuild/tools/win32/lib/libc.dll.a -o netsurf
 
-echo "Ignore the errors above (for now...)"
-echo "_netsurf is your executable. Enjoy."
+if [ "$?" -eq "0" ]; then
+    objcopy -O binary _netsurf
+    if [ "$?" -eq "0" ]; then
+	echo
+	echo "_netsurf is your executable. Run it on KolibriOS. Enjoy."
+    fi
+else
+    echo "Something went wrong with the linker process. Failed to build executable."
+fi
