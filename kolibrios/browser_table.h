7de409222737748bd7456a3ae185b611cba5fc47
Ashish Gupta <ashmew2@gmail.com>
2016-03-03 21:59:36 +0000

    add quit() function to browser_table

--- kolibrios/browser_table.h
+++ kolibrios/browser_table.h
@@ -2,12 +2,19 @@
 /* Part about Browser table. Will contain all functions required as well. */
 /* ------------------------------ */
 nserror kolibri_schedule_cb(int t, void (*callback)(void *p), void *p);
+void kolibri_browser_quit(void);
 
 static struct gui_browser_table kolibri_browser_table = {
     .schedule = kolibri_schedule_cb,
+    .quit = kolibri_browser_quit,
     };
 
 nserror kolibri_schedule_cb(int t, void (*callback)(void *p), void *p)
 {
   return NSERROR_OK;
+}
+
+void kolibri_browser_quit(void)
+{
+  debug_board_write_str("Netsurf is shutting down Normally..GoodBye.");
 }
