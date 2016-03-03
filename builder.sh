83a3a3fa6935f0b00d097d65e3155055ee2347e7
Ashish Gupta <ashmew2@gmail.com>
2016-03-03 21:50:09 +0000

    Make builder more informative and resilient

--- builder.sh
+++ builder.sh
@@ -4,14 +4,19 @@ cd build-Linux-kolibrios
 
 cp ../kolibrios/*obj .
 
-kos32-ld *.o ~/ofiles/*.o *.obj -T/home/bob/kolibrios/contrib/sdk/sources/newlib/libc/app.lds -nostdlib -static --image-base 0 -L/home/autobuild/tools/win32/mingw32/lib -lgcc /home/autobuild/tools/win32/lib/libz.a /home/autobuild/tools/win32/lib/libdll.a /home/autobuild/tools/win32/lib/libapp.a /home/autobuild/tools/win32/lib/libc.dll.a -o netsurf
+kos32-ld *.o ~/ofiles/*.o *.obj -T/home/bob/kolibrios/contrib/sdk/sources/newlib/libc/app.lds -nostdlib -static --image-base 0 -L/home/autobuild/tools/win32/mingw32/lib -lgcc /home/autobuild/tools/win32/lib/libz.a /home/autobuild/tools/win32/lib/libdll.a /home/autobuild/tools/win32/lib/libapp.a /home/autobuild/tools/win32/lib/libc.dll.a -o _netsurf -Map=../nsbin.map
 
 if [ "$?" -eq "0" ]; then
-    objcopy -O binary _netsurf
+    objcopy -O binary _netsurf nsbin
+    cp nsbin ../nsbin
     if [ "$?" -eq "0" ]; then
 	echo
-	echo "_netsurf is your executable. Run it on KolibriOS. Enjoy."
+	echo "nsbin is your executable. Run it on KolibriOS. Enjoy."
     fi
 else
     echo "Something went wrong with the linker process. Failed to build executable."
+    exit -1
 fi
+
+date
+ls -l ../nsbin ../nsbin.map
