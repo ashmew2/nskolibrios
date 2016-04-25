8d5bf990f6ab7560ca7938dec9d62dbbc6f30fed
Ashish Gupta <ashmew2@gmail.com>
2016-04-25 20:59:55 +0000

    Add env.sh for our setup

---
+++ env.sh
@@ -0,0 +1,3 @@
+export PKG_CONFIG_PATH=${HOME}/dev-netsurf/workspace/netsurf/inst/lib/pkgconfig::
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HOME}/dev-netsurf/workspace/netsurf/inst/lib
+export PREFIX=${HOME}/dev-netsurf/workspace/netsurf/inst
