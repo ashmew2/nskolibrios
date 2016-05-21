5d59630b7f69b4b2ee47d069ed14fc74c3e737b6
Ashish Gupta <ashmew2@gmail.com>
2016-05-21 16:33:23 +0000

    add http_send function to abstract retries

--- netsurf/kolibrios/kolibri_http.c
+++ netsurf/kolibrios/kolibri_http.c
@@ -9,3 +9,28 @@ int kolibri_http_init(void)
   else
     return 1;
 }
+
+void http_send(struct http_msg *handle, char *data, int remaining_length) {
+
+  int sent = 0;
+  int total_sent = 0;
+  int retries_left = 5;
+
+  while(remaining_length > 0) {
+    sent = http_send_asm(handle, data + total_sent, remaining_length);
+
+    if(sent == -1) {
+      --retries_left;
+
+      if(retries_left)
+	continue;
+      else {
+	debug_board_write_str("ERROR: HTTP LIBRARY : Could not send data.\n");
+	__asm__ __volatile__("int3");
+      }
+    }
+
+    remaining_length -= sent;
+    total_sent += sent;
+  }
+}
